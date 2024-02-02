4dc5cee613beffa5fbda2babde0e1c71ba102ad4
MD. Billal <157817847+Aiden710@users.noreply.github.com>
2024-02-03 02:34:30 +0600

    Add files via upload

---
+++ projectA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char choice;
+    cout<<"\n1- Login";
+    cout<<"\n1- sing up";
+    cout<<"\n1- Forgot Password";
+    cout<<"\n1- Exit";
+    cout<<"\nEnter Your Choice:: ";
+    cin>>choice;
+
+    switc(choice)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
